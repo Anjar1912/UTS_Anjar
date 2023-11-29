dd73a4a15fd89572a6622005413b872c2284df05
Anjar1912 <anjar.1912@students.amikom.ac.id>
2023-11-29 14:44:39 +0700

    Create App.java

---
+++ app/src/main/java/com/aryobimo/uts/App.java
@@ -0,0 +1,14 @@
+package com.aryobimo.uts;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    public String bahasa = "Indonesia";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
